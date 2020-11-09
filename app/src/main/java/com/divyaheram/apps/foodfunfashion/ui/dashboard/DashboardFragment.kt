2ca3eadd61dccf7854f7ec70cc67226d150b2ea8
bschandramohan <bschandramohan@gmail.com>
2020-11-08 19:59:00 -0800

    Handle SSL error and load https page; Not for production - ignoring all SSL errors
    Proper handling here:
    https://stackoverflow.com/questions/36050741/webview-avoid-security-alert-from-google-play-upon-implementation-of-onreceiveds

--- app/src/main/java/com/divyaheram/apps/foodfunfashion/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/divyaheram/apps/foodfunfashion/ui/dashboard/DashboardFragment.kt
@@ -2,10 +2,12 @@ package com.divyaheram.apps.foodfunfashion.ui.dashboard
 
 import android.content.Intent
 import android.net.Uri
+import android.net.http.SslError
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.SslErrorHandler
 import android.webkit.WebChromeClient
 import android.webkit.WebView
 import android.webkit.WebViewClient
@@ -15,7 +17,6 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.divyaheram.apps.foodfunfashion.R
-import kotlinx.android.synthetic.main.fragment_dashboard.*
 
 class DashboardFragment : Fragment() {
 
@@ -40,11 +41,16 @@ class DashboardFragment : Fragment() {
             it.javaScriptEnabled = true
             it.domStorageEnabled = true
         }
-        videoView.webViewClient = WebViewClient()
+        videoView.webViewClient = object : WebViewClient() {
+            override fun onReceivedSslError(v: WebView, handler: SslErrorHandler, er: SslError) {
+                handler.proceed()
+            }
+        }
+
         videoView.webChromeClient = WebChromeClient()
 
 //        videoView.loadUrl("https://www.google.com/")
-         videoView.loadUrl("https://www.youtube.com/")
+        videoView.loadUrl("https://www.youtube.com/")
 //        videoView.loadUrl("https://www.youtube.com/watch?v=FTS_p6bA2qs")
 
         val nextVideo = root.findViewById<Button>(R.id.nextVideo)
