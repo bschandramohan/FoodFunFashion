997d3a035e62959aab7fc4a01b129eb3f55805d6
bschandramohan <bschandramohan@gmail.com>
2020-11-08 19:57:05 -0800

    Proper message for no notifications

--- app/src/main/java/com/divyaheram/apps/foodfunfashion/ui/notifications/NotificationsViewModel.kt
+++ app/src/main/java/com/divyaheram/apps/foodfunfashion/ui/notifications/NotificationsViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class NotificationsViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "No new notifications currently"
     }
     val text: LiveData<String> = _text
 }
